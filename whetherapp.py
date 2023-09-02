0ed46ea2bf27061f0f27a064241f96c6c98c3739
Programmed-Boy <139626527+muhammad-hassan1112@users.noreply.github.com>
2023-09-02 22:08:09 +0500

    Update whetherapp.py
    This is the Python program which uses api.openweathermap for getting certain information about the weather and also put your api key of the api.openweathermap

--- whetherapp.py
+++ whetherapp.py
@@ -7,7 +7,6 @@ while True:
         base_url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}'
 
         response = requests.get(base_url)
-        print(response.text)
         data=json.loads(response.text)
         print(f'cordinates are: {data["coord"]}')
         print(f'tempreature is: {data["main"]["temp"]}°C')
